25d62ba48a1aa4ff470e8c5f3daad00531fbf4b2
Nahur Streit <nahur.streit12@gmail.com>
2024-09-17 14:10:48 -0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,20 +3,16 @@ import Navbar from "@/components/Navbar/Navbar";
 
 export default function NotFound() {
   return (
-	<body
-    className={`antialiased`}
-    style={{ paddingTop: '4rem', paddingBottom: '4rem' }}
-    >
-        <Header />
-		<div className='flex flex-col gap-4 pb-20'>
-			<span style={{color: 'rgba(239, 83, 86, 1)', fontSize:'30px', fontWeight:'700'}}>Ops!</span>
-			<div className=''>
-				<img src='not-found.svg' alt='Página não encontrada'/>
-			</div>
-			<span style={{color: 'rgba(239, 83, 86, 1)', fontSize:'20px', fontWeight:'500'}}>Página não encontrada...</span>
-		</div>
-		
-        <Navbar />
-    </body>
-  )
+    <div className="flex flex-col justify-between min-h-screen antialiased">
+      <Header />
+      <div className='flex flex-col items-center justify-center flex-grow gap-4 text-center pb-12 pt-8'>
+        <span className="text-red-600 text-3xl font-bold">Ops!</span>
+        <div>
+          <img src='not-found.svg' alt='Página não encontrada' />
+        </div>
+        <span className="text-red-600 text-xl font-medium">Página não encontrada...</span>
+      </div>
+      <Navbar />
+    </div>
+  );
 }
